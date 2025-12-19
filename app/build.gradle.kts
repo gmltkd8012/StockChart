4df9dbdcce28202d5690e8731278d7bba97ccb67
LeeHeesang <gmltkd8012@naver.com>
2025-12-19 16:12:22 +0900

    [LeeCoder] Feat: APK Build 추가 Gradle 설정 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,8 @@
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.text.SimpleDateFormat
+import java.util.Date
+
 plugins {
     alias(libs.plugins.application)
     alias(libs.plugins.app.flavor)
@@ -8,6 +13,9 @@ plugins {
 android {
     namespace = "com.leecoder.stockchart"
 
+    val appKeyAlias = System.getenv("APP_KEY_ALIAS") ?: gradleLocalProperties(rootDir, providers).getProperty("app_key_alias")
+    val appKeyPassword = System.getenv("APP_KEY_PASSWORD") ?: gradleLocalProperties(rootDir, providers).getProperty("app_key_pw")
+
     defaultConfig {
         applicationId = "com.leecoder.stockchart"
         targetSdk = libs.versions.compileSdk.get().toInt()
@@ -15,15 +23,45 @@ android {
         versionName = libs.versions.appVersionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "APP_KEY_ALIAS", "\"$appKeyAlias\"")
+        buildConfigField("String", "APP_KEY_PASSWORD", "\"$appKeyPassword\"")
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("../stockchart.keystore.jks")
+            storePassword = appKeyPassword
+            keyAlias = appKeyAlias
+            keyPassword = appKeyPassword
+        }
     }
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+
+    applicationVariants.configureEach {
+        val dateFormat = SimpleDateFormat("yyyyMMdd")
+        val today = dateFormat.format(Date())
+        val appName = "bollinger_alarm"
+        val versionName = defaultConfig.versionName.orEmpty()
+
+        this.outputs.configureEach {
+            buildOutputs.forEach { _ ->
+                val archivesBaseName = "$appName-$flavorName-${buildType.name}-v$versionName-$today.apk"
+                val variantOutputImpl = this as BaseVariantOutputImpl
+                variantOutputImpl.outputFileName = archivesBaseName
+            }
         }
     }
 }
