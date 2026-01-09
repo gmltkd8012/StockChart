0eee89bfd329b0eb73d2195542a01b103372cab4
LeeHeesang <gmltkd8012@naver.com>
2026-01-10 01:24:36 +0900

    [LeeCoder] Modify: 볼린저 하한가 달성 UI 표기 실제 데이터 연동

--- feature/app/src/main/kotlin/com/leecoder/stockchart/app/screen/SubscribeScreen.kt
+++ feature/app/src/main/kotlin/com/leecoder/stockchart/app/screen/SubscribeScreen.kt
@@ -1,6 +1,5 @@
 package com.leecoder.stockchart.app.screen
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,16 +10,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,12 +27,6 @@ import com.leecoder.stockchart.app.viewmodel.SubscribeViewModel
 import com.leecoder.stockchart.design_system.component.BaseRateBox
 import com.leecoder.stockchart.design_system.component.BaseRegistedBox
 import com.leecoder.stockchart.design_system.component.BaseStockBox
-import com.leecoder.stockchart.design_system.component.BaseSymbolItem
-import com.leecoder.stockchart.model.exchange.ExchangeRateData
-import com.leecoder.stockchart.model.stock.StockTick
-import com.leecoder.stockchart.model.ui.NasdaqUiData
-import com.leecoder.stockchart.model.ui.StockUiData
-import com.leecoder.stockchart.util.extension.convertToDouble
 
 @Composable
 fun SubscribeScreen(
@@ -47,8 +35,7 @@ fun SubscribeScreen(
 ) {
     val state by viewModel.state.collectAsState()
     val stockTick = state.tick
-
-    var isBool by remember { mutableStateOf(false) }
+    val bollingerAlertCodes = state.bollingerLowerAlertCodes
 
     Box(
         modifier = Modifier
@@ -121,9 +108,10 @@ fun SubscribeScreen(
                             currencyUSD = state.currency,
                             date = stockTick.kymd + stockTick.khms,
                             onDelete = { code, name ->
-                                isBool = true
+                                onDeletedSymbol(code, name)
                             },
-                            isBollingerLowerAlert = isBool
+                            // 볼린저 하한가 달성 종목인지 확인
+                            isBollingerLowerAlert = bollingerAlertCodes.contains(stockTick.code)
                         )
 
                         Spacer(Modifier.height(12.dp))
